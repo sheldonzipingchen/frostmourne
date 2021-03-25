6fb63dfeb49a13d8e42fa2443d935c82caa8c3d6
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2021-03-25 14:08:15 +0800

    rabbit mq 配置信息
    rabbit mq 配置信息

---
+++ frostmq/msg_queue.go
@@ -0,0 +1,56 @@
+package frostmq
+
+import (
+	"fmt"
+	"frostmourne/config"
+	"frostmourne/loglib"
+	"github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
+)
+
+type FrostMQClient interface {
+}
+
+type frostMQClient struct {
+	Username string
+	Password string
+	Addr     string
+	Port     string
+}
+
+var conn *amqp.Connection
+var msgQueueLog = loglib.GetLog()
+
+func Init() {
+	c := config.GetConfig()
+
+	host := c.GetString("amqp.host")
+	if host == "" {
+		host = "localhost"
+	}
+	port := c.GetString("amqp.port")
+	if port == "" {
+		port = "5672"
+	}
+	username := c.GetString("amqp.username")
+	if username == "" {
+		username = "guest"
+	}
+	password := c.GetString("amqp.password")
+	if password == "" {
+		password = "guest"
+	}
+
+	amqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, host, port)
+	var err error
+	conn, err = amqp.Dial(amqpURI)
+	if err != nil {
+		msgQueueLog.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("AMQP Connected fail")
+	}
+}
+
+func NewFrostMQClient() FrostMQClient {
+	return &frostMQClient{}
+}
